27cef6ec9d7cbbb34841ea9d05ae2d1bc165fb16
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2023-09-20 13:31:36 +0200

    c)

--- src/App.java
+++ src/App.java
@@ -10,6 +10,13 @@ public class App {
 
         System.out.println("Write a letter that you want to locate in your text: ");
         String locate = tangentbord.nextLine();
-        System.out.println("The position of letter \"" +locate+"\" is in the: "+text.indexOf(locate)+" position");
+        System.out.println("The first position of the letter \"" +locate+"\" is in the: "+text.indexOf(locate)+" position");
+        
+        System.out.println("You will take out a part of the text.");
+        System.out.println("Type the number of the position you want to start from: ");
+        int first_position = tangentbord.nextInt();
+        System.out.println("Type the number of the position you want to end on: ");
+        int second_position = tangentbord.nextInt();
+        System.out.println("This part of the text is: \""+text.substring(first_position, second_position)+"\"");
     }
 }
