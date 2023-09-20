1b6e2857b78a116c8008e54f7652366d153b8d3e
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2023-09-20 13:03:32 +0200

    b)

--- src/App.java
+++ src/App.java
@@ -8,7 +8,8 @@ public class App {
         String text = tangentbord.nextLine();
         System.out.println("The length of \""+text+"\" is: "+text.length()+" letters");
 
-
-
+        System.out.println("Write a letter that you want to locate in your text: ");
+        String locate = tangentbord.nextLine();
+        System.out.println("The position of letter \"" +locate+"\" is in the: "+text.indexOf(locate)+" position");
     }
 }
