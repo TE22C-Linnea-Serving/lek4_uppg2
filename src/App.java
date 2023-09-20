f0aad398dff48b6e7211a6dc50e5f2046f19b738
Linnea Serving <linnea.serving@elev..ga.ntig.se>
2023-09-20 10:57:37 +0200

    a)

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    public static void main(String[] args) {
+        Scanner tangentbord = new Scanner(System.in);
+        System.out.println("Skriv in en text: ");
+        String text = tangentbord.nextLine();
+        System.out.println("The length of \""+text+"\" is: "+text.length()+" letters");
+
+
+
     }
 }
